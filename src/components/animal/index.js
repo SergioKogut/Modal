8c820fb8f3df9109a880ede3e6b424a9d1e83f93
Anonymous <user@rivne.itstep.org>
2019-06-20 20:56:02 +0300

    add new spinner

--- src/components/animal/index.js
+++ src/components/animal/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {withRouter} from "react-router-dom"
+import { withRouter } from "react-router-dom"
 import * as animalActions from './reducer';
 import get from 'lodash.get';
 import propTypes from 'prop-types';
+import SpinnerWidget from '../spinner';
 
 class AnimalWidgetContainer extends Component {
     state = {}
@@ -11,8 +12,8 @@ class AnimalWidgetContainer extends Component {
     componentDidMount() {
         this.props.getListData();
     }
-    redirectToAnimal=(e)=>{
-        const {history}=this.props;
+    redirectToAnimal = (e) => {
+        const { history } = this.props;
         e.preventDefault();
         console.log('-----переходимо на сторінку додавання----');
         history.push('animal/add');
@@ -21,11 +22,12 @@ class AnimalWidgetContainer extends Component {
     render() {
         console.log('----state-----', this.state);
         console.log('----Props-----', this.props);
+        const { isListLoading } = this.props;
         const listContent = this.props.list.map(item => {
             return (
                 <div key={item.id} className="col-lg-3 col-md-4 col-6">
                     <div className="d-block mb-4 h-100">
-                        <img className="img-fluid img-thumbnail" style={{"boxShadow": "0 0 5px 2px" }} src={item.image} alt="" />
+                        <img className="img-fluid img-thumbnail" style={{ "boxShadow": "0 0 5px 2px" }} src={item.image} alt="" />
                         <b> {item.name}    </b>
                     </div>
                 </div>
@@ -35,7 +37,7 @@ class AnimalWidgetContainer extends Component {
             <div>
                 <div className="container">
 
-                    <button  className="btn btn-info" onClick={this.redirectToAnimal}> Додати тварину</button>
+                    <button className="btn btn-info" onClick={this.redirectToAnimal}> Додати тварину</button>
                     <h1 className="font-weight-light text-center text-lg-left mt-4 mb-0">Thumbnail Gallery</h1>
 
                     <hr className="mt-2 mb-5" />
@@ -46,6 +48,7 @@ class AnimalWidgetContainer extends Component {
                     </div>
 
                 </div>
+                <SpinnerWidget loading={isListLoading} />
             </div>
         );
     }
@@ -67,12 +70,12 @@ const mapDispatch = (dispatch) => {
 }
 
 AnimalWidgetContainer.propTypes =
-{
-history:propTypes.object.isRequired,
-list:propTypes.array.isRequired,
-isListError:propTypes.bool.isRequired,
-isListLoading:propTypes.bool.isRequired
-}
+    {
+        history: propTypes.object.isRequired,
+        list: propTypes.array.isRequired,
+        isListError: propTypes.bool.isRequired,
+        isListLoading: propTypes.bool.isRequired
+    }
 
 const AnimalWidget = withRouter(connect(mapState, mapDispatch)(AnimalWidgetContainer));
 
